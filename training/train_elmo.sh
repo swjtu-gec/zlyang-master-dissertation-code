d90875020978706f700d938c6e8c09781e57fc53
yangzonglin <806765278@qq.com>
2019-01-24 21:09:45 +0800

    Use standard hyper parameters setting to train `fconv_elmo` model.

--- training/train_elmo.sh
+++ training/train_elmo.sh
@@ -17,5 +17,7 @@ CUDA_VISIBLE_DEVICES=0 python ${FAIRSEQPY}/train.py \
     -a fconv_elmo \
     --clip-norm 0.1 --min-lr 1e-4 \
     --max-epoch 100 --max-tokens 1000 --max-sentences 10 \
-    --no-progress-bar --seed ${SEED}
+    --no-progress-bar --seed ${SEED} \
+    --use-other-embed --num-output-repr 1 \
+    --merge-mode "concat" --token-embed-dim 500
 
