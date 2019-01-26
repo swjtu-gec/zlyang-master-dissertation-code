42e45d6e5b2ba427a9e69b6902d51c3a51fca360
yangzonglin <806765278@qq.com>
2019-01-26 23:00:11 +0800

    Increase batch size and use two layers of ELMo.

--- training/train_elmo.sh
+++ training/train_elmo.sh
@@ -16,8 +16,8 @@ CUDA_VISIBLE_DEVICES=0 python ${FAIRSEQPY}/train.py \
     --save-dir ${OUT_DIR} \
     -a fconv_elmo \
     --clip-norm 0.1 --min-lr 1e-4 \
-    --max-epoch 100 --max-tokens 1000 --max-sentences 10 \
+    --max-epoch 100 --max-tokens 5000 --max-sentences 50 \
     --no-progress-bar --seed ${SEED} \
-    --use-other-embed --num-output-repr 1 \
+    --use-other-embed --num-output-repr 2 \
     --merge-mode "concat" --token-embed-dim 500
 
