f4264f9f4c120ecc8211c2b5e2bd9af08420c347
yangzonglin <806765278@qq.com>
2019-03-02 00:58:48 +0800

    Script used to train fconv+BPE model.

---
+++ training/train_zh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+set -e
+
+source ../paths.sh
+
+SEED=1000
+DATA_BIN_DIR=zh-processed/bin
+
+OUT_DIR=models/fconv_zh/model${SEED}/
+mkdir -p ${OUT_DIR}
+
+CUDA_VISIBLE_DEVICES="0" python ${FAIRSEQPY}/train.py \
+    ${DATA_BIN_DIR} \
+    --save-dir ${OUT_DIR} \
+    -a fconv \
+    --encoder-embed-dim 500 \
+    --decoder-embed-dim 500 --decoder-out-embed-dim 500 \
+    --dropout='0.2' --clip-norm=0.1 --lr 0.25 --min-lr 1e-4 \
+    --encoder-layers '[(1024,3)] * 7' --decoder-layers '[(1024,3)] * 7' \
+    --momentum 0.99 --max-epoch 100 \
+    --max-tokens 3000 --max-sentences 30 \
+    --no-progress-bar --seed ${SEED}
+
