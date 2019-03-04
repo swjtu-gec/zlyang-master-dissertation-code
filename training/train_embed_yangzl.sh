6e79ec275be168a803258fa328a99bfae49132a1
yangzonglin <806765278@qq.com>
2019-03-05 01:28:20 +0800

    1. Used GPU devices should be also specified by command line argument. 2. Add `--fp16` flag to speed up training.

--- training/train_embed_yangzl.sh
+++ training/train_embed_yangzl.sh
@@ -6,17 +6,18 @@ set -e
 
 source ../paths.sh
 
-if [[ $# != 6 ]]; then
-    echo "Usage: `basename $0` <dir to bin data> <model_name(e.g: fconv_zh_bpe_embed)> <embed_file> <random seed> <max tokens> <max sentences>"
+if [[ $# != 7 ]]; then
+    echo "Usage: `basename $0` <dir to bin data> <GPU device id to use(e.g: '0, 1, 2')> <model_name(e.g: fconv_zh_bpe_embed)> <embed_file> <random seed> <max tokens> <max sentences>"
     exit -1
 fi
 
 DATA_BIN_DIR=$1
-model_name=$2
-EMBED_URL=$3
-SEED=$4
-MAX_TOKENS=$5
-MAX_SENS=$6
+use_gpus=$2
+model_name=$3
+EMBED_URL=$4
+SEED=$5
+MAX_TOKENS=$6
+MAX_SENS=$7
 
 if [[ ! -f "${EMBED_URL}" ]]; then
     echo "cannot find embedding file in ${EMBED_URL}"
@@ -26,10 +27,11 @@ fi
 OUT_DIR=models/${model_name}/model${SEED}/
 mkdir -p ${OUT_DIR}
 
-CUDA_VISIBLE_DEVICES="0" python ${FAIRSEQPY}/train.py \
+CUDA_VISIBLE_DEVICES=${use_gpus} python ${FAIRSEQPY}/train.py \
     ${DATA_BIN_DIR} \
     --save-dir ${OUT_DIR} \
     -a fconv \
+    --fp16 \
     --encoder-embed-dim 500 --encoder-embed-path ${EMBED_URL} \
     --decoder-embed-dim 500 --decoder-embed-path ${EMBED_URL} --decoder-out-embed-dim 500 \
     --dropout 0.2 --clip-norm 0.1 --lr 0.25 --min-lr 1e-4 \
