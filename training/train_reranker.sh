52aae8b18eca3315ceea0c3ac924a0b5cb3949bc
yangzonglin <806765278@qq.com>
2019-03-10 03:18:10 +0800

    1. General script to train re-ranker. 2. Some code optimization. 3. Compute re-ranker training time consuming.

--- training/train_reranker.sh
+++ training/train_reranker.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+# This script used to train re-ranker.
+
 set -e
 set -x
 
 source ../paths.sh
 
-if [[ $# -ge 5 ]]; then
-    output_dir=$1
-    device=$2
-    model_path=$3
-    reranker_feats=$4
-    moses_path=$5
+if [[ $# -ge 8 ]]; then
+    dev_data_dir=$1
+    output_dir=$2
+    device=$3
+    model_path=$4
+    reranker_feats=$5
+    moses_path=$6
+    DATA_BIN_DIR=$7
+    BPE_MODEL_DIR=$8
+    if [[ "${reranker_feats}" == "eolm" ]]; then
+        lm_url=$9
+    fi
 else
-    echo "Please specify the paths to the input_file and output rerdirectory"
-    echo "Usage: `basename $0` <output_dir> <gpu-device-num(e.g: 0)> <path to model_file/dir> <features, e.g 'eo' or 'eolm'> <path-to-moses>"   >&2
+    echo "Usage: `basename $0` <dir to dev data> <output_dir> <GPU device id to use(e.g: '0, 1, 2')> <path to model_file/dir> <features, e.g: 'eo' or 'eolm'> <path-to-moses> <dir to bin data> <dir to BPE model> [optional args: <trained language model's url>]"
+    exit -1
 fi
 
 
@@ -30,6 +38,7 @@ elif [[ -f "$model_path" ]]; then
     models=${model_path}
 elif [[ ! -e "$model_path" ]]; then
     echo "Model path not found: $model_path"
+    exit -2
 fi
 
 ###############
@@ -41,38 +50,45 @@ mkdir -p ${TRAIN_DIR}
 echo "[weight]" > ${TRAIN_DIR}/rerank_config.ini
 echo "F0= 0.5" >> ${TRAIN_DIR}/rerank_config.ini
 echo "EditOps0= 0.2 0.2 0.2" >> ${TRAIN_DIR}/rerank_config.ini
-if [[ ${reranker_feats} == "eolm" ]]; then
+if [[ "${reranker_feats}" == "eolm" ]]; then
     echo "LM0= 0.5" >> ${TRAIN_DIR}/rerank_config.ini
     echo "WordPenalty0= -1" >> ${TRAIN_DIR}/rerank_config.ini
 fi
 
-if [[ ${reranker_feats} == "eo" ]]; then
+if [[ "${reranker_feats}" == "eo" ]]; then
     featstring="EditOps(name='EditOps0')"
-elif [[ ${reranker_feats} == "eolm" ]]; then
-    LM_PATH=${MODEL_DIR}/lm/94Bcclm.trie
-    if [[ ! -f ${LM_PATH} ]]; then
-        mkdir -p ${MODEL_DIR}/lm
-        curl -L -o ${MODEL_DIR}/lm/94Bcclm.trie https://tinyurl.com/yb4jutxq/lm/94Bcclm.trie
+elif [[ "${reranker_feats}" == "eolm" ]]; then
+    if [[ ! -f ${lm_url} ]]; then
+        echo "Language model not found: ${lm_url}"
+        exit -3
     fi
-    featstring="EditOps(name='EditOps0'), LM('LM0', '$MODEL_DIR/lm/94Bcclm.trie', normalize=False), WordPenalty(name='WordPenalty0')"
+    featstring="EditOps(name='EditOps0'), LM('LM0', '$lm_url', normalize=False), WordPenalty(name='WordPenalty0')"
+else
+    echo "Unknown re-ranker features string. got ${reranker_feats}"
+    exit -4
 fi
 
+train_reranker_starttime=$(date +%s)
 
-${SCRIPTS_DIR}/apply_bpe.py -c models/bpe_model/train.bpe.model < processed/dev.input.txt > ${output_dir}/dev.input.bpe.txt
+${SCRIPTS_DIR}/apply_bpe.py -c ${BPE_MODEL_DIR}/train.bpe.model < ${dev_data_dir}/dev.input.txt > ${output_dir}/dev.input.bpe.txt
 
-CUDA_VISIBLE_DEVICES=${device} python ${FAIRSEQPY}/interactive.py \
+CUDA_VISIBLE_DEVICES="${device}" python ${FAIRSEQPY}/interactive.py \
     --no-progress-bar --path ${models} \
     --beam ${beam} --nbest ${beam} \
-    processed/bin < ${output_dir}/dev.input.bpe.txt > ${output_dir}/dev.output.bpe.nbest.txt
+    ${DATA_BIN_DIR} < ${output_dir}/dev.input.bpe.txt > ${output_dir}/dev.output.bpe.nbest.txt
 
 # reformating the nbest file
 ${SCRIPTS_DIR}/nbest_reformat.py -i ${output_dir}/dev.output.bpe.nbest.txt --debpe > ${output_dir}/dev.output.tok.nbest.reformat.txt
 
 # augmenting the dev nbest
-${NBEST_RERANKER}/augmenter.py -s processed/dev.input.txt -i ${output_dir}/dev.output.tok.nbest.reformat.txt -o ${output_dir}/dev.output.tok.nbest.reformat.augmented.txt -f "$featstring"
+${NBEST_RERANKER}/augmenter.py -s ${dev_data_dir}/dev.input.txt -i ${output_dir}/dev.output.tok.nbest.reformat.txt -o ${output_dir}/dev.output.tok.nbest.reformat.augmented.txt -f "$featstring"
 
 # training the nbest to obtain the weights
-${NBEST_RERANKER}/train.py -i ${output_dir}/dev.output.tok.nbest.reformat.augmented.txt -r processed/dev.m2 -c ${TRAIN_DIR}/rerank_config.ini --threads 12 --tuning-metric m2 --predictable-seed -o ${TRAIN_DIR} --moses-dir ${moses_path} --no-add-weight
+${NBEST_RERANKER}/train.py -i ${output_dir}/dev.output.tok.nbest.reformat.augmented.txt -r ${dev_data_dir}/dev.m2 -c ${TRAIN_DIR}/rerank_config.ini --threads ${threads} --tuning-metric m2 --predictable-seed -o ${TRAIN_DIR} --moses-dir ${moses_path} --no-add-weight
 
 cp ${TRAIN_DIR}/weights.txt ${output_dir}/weights.txt
+
+train_reranker_endtime=$(date +%s)
+cost=$((train_reranker_endtime - train_reranker_starttime))
+echo "re-ranker training end. cost ${cost}s"
 
