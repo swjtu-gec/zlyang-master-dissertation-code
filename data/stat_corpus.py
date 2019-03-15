efbed1136afd9631db67d55043f98f144853fe1f
yangzonglin <806765278@qq.com>
2019-03-16 00:43:23 +0800

    add more control argument

--- data/stat_corpus.py
+++ data/stat_corpus.py
@@ -16,8 +16,10 @@ from utils import tools
 
 
 def main(args):
-    stat_words_distribution(args.corpus_fname, args.encoding)
-    stat_sens_distribution(args.corpus_fname, args.encoding)
+    if args.stat_words:
+        stat_words_distribution(args.corpus_fname, args.encoding)
+    elif args.stat_sens:
+        stat_sens_distribution(args.corpus_fname, args.short, args.long, args.encoding)
 
 
 def stat_words_distribution(corpus_fname, encoding='utf-8'):
@@ -46,20 +48,22 @@ def stat_words_distribution(corpus_fname, encoding='utf-8'):
               .format(cnt, times, cnt / vocab_size * 100, cnt*times / all_tokens_num * 100))
 
 
-def stat_sens_distribution(corpus_fname, encoding='utf-8'):
-    line_cnt = reader.count_lines(corpus_fname, encoding)
+def stat_sens_distribution(corpus_fname, short, long, encoding='utf-8'):
+    lines_cnt = reader.count_lines(corpus_fname, encoding)
     print('=================== sentences distribution ========================')
     len_stat = {}
     with open(corpus_fname, 'r', encoding=encoding) as file:
         for line in file:
             sen_len = len(line.split())
+            if sen_len < short or sen_len > long:
+                print(line)
             sen_len = math.ceil(sen_len / 10) * 10
             if len_stat.get(sen_len) is not None:
                 len_stat[sen_len] += 1
             else:
                 len_stat[sen_len] = 1
     len_stat = list(len_stat.items())
-    assert line_cnt == sum(map(lambda x: x[1], len_stat))
+    assert lines_cnt == sum(map(lambda x: x[1], len_stat))
     len_stat.sort(key=lambda x: x[0])
     len_stat = dict(len_stat)
     for length, cnt in len_stat.items():
@@ -70,6 +74,10 @@ def stat_sens_distribution(corpus_fname, encoding='utf-8'):
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--corpus-fname', type=str, metavar='STR', required=True, help='corpus filename')
+    parser.add_argument('--stat-words', action='store_true', help='whether to show words stat info')
+    parser.add_argument('--stat-sens', action='store_true', help='whether to show sens len stat info')
+    parser.add_argument('--short', type=int, metavar='N', help='sen len < `short` will be shown')
+    parser.add_argument('--long', type=int, metavar='N', help='sen len > `long` will be shown')
     parser.add_argument('--encoding', type=str, metavar='STR', help='open and save encoding')
     parser.add_argument('--debug', action='store_true', help='whether to show more info for debug')
     return parser
@@ -78,6 +86,10 @@ def create_parser():
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
+    args.stat_words = getattr(args, 'stat_words', False)
+    args.stat_sens = getattr(args, 'stat_sens', False)
+    args.short = my_getattr(args, 'short', 2)
+    args.long = my_getattr(args, 'long', 100)
     args.encoding = my_getattr(args, 'encoding', 'utf-8')
     args.debug = getattr(args, 'debug', False)
 
