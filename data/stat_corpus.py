1775ad8cd16a3bc8f9daed8345c639884613bd3f
yangzonglin <806765278@qq.com>
2019-03-17 21:12:36 +0800

    modify variable name and show vocabulary size

--- data/stat_corpus.py
+++ data/stat_corpus.py
@@ -1,5 +1,5 @@
 """
-This script used to statistic corpus information including distribution of words and sentences.
+This script used to statistic corpus information including distribution of tokens and sentences.
 """
 import argparse
 import math
@@ -16,14 +16,14 @@ from utils import tools
 
 
 def main(args):
-    if args.stat_words:
-        stat_words_distribution(args.corpus_fname, args.encoding)
+    if args.stat_tokens:
+        stat_tokens_distribution(args.corpus_fname, args.encoding)
     elif args.stat_sens:
         stat_sens_distribution(args.corpus_fname, args.short, args.long, args.encoding)
 
 
-def stat_words_distribution(corpus_fname, encoding='utf-8'):
-    print('=================== words distribution ========================')
+def stat_tokens_distribution(corpus_fname, encoding='utf-8'):
+    print('=================== tokens distribution ========================')
     with open(corpus_fname, 'r', encoding=encoding) as file:
         tcounts = Counter(file.read().split())
     tcounts = list(tcounts.items())
@@ -34,6 +34,7 @@ def stat_words_distribution(corpus_fname, encoding='utf-8'):
         else:
             times_stat[count] += 1
     vocab_size = len(tcounts)
+    print('vocab size:', vocab_size)
     all_distinct_tokens = reader.read_tokens(corpus_fname, encoding)
     assert vocab_size == len(all_distinct_tokens), 'len of tcounts != corpus distinct tokens count'
     times_stat = list(times_stat.items())
@@ -43,7 +44,7 @@ def stat_words_distribution(corpus_fname, encoding='utf-8'):
     print('all tokens num:', all_tokens_num)
     times_stat.sort(key=lambda x: x[1], reverse=True)
     for times, cnt in times_stat:
-        print('{0:6} words occur {1:10} times, the ratio of vocab_size is {2:>10.6f}% '
+        print('{0:6} tokens occur {1:10} times, the ratio of vocab_size is {2:>10.6f}% '
               '| the ratio of all tokens num is {3:>10.6f}%'
               .format(cnt, times, cnt / vocab_size * 100, cnt*times / all_tokens_num * 100))
 
@@ -74,7 +75,7 @@ def stat_sens_distribution(corpus_fname, short, long, encoding='utf-8'):
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--corpus-fname', type=str, metavar='STR', required=True, help='corpus filename')
-    parser.add_argument('--stat-words', action='store_true', help='whether to show words stat info')
+    parser.add_argument('--stat-tokens', action='store_true', help='whether to show tokens stat info')
     parser.add_argument('--stat-sens', action='store_true', help='whether to show sens len stat info')
     parser.add_argument('--short', type=int, metavar='N', help='sen len < `short` will be shown')
     parser.add_argument('--long', type=int, metavar='N', help='sen len > `long` will be shown')
@@ -86,7 +87,7 @@ def create_parser():
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    args.stat_words = getattr(args, 'stat_words', False)
+    args.stat_tokens = getattr(args, 'stat_tokens', False)
     args.stat_sens = getattr(args, 'stat_sens', False)
     args.short = my_getattr(args, 'short', 2)
     args.long = my_getattr(args, 'long', 100)
