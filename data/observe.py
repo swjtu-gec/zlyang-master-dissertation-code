dc796fafd747e6c34804a30d9075c6ed190d0318
yangzonglin <806765278@qq.com>
2019-03-18 20:24:52 +0800

    control to show which illegal sentence pairs

--- data/observe.py
+++ data/observe.py
@@ -34,27 +34,35 @@ def sim_two_files(first_file, second_file, show_diff, encoding):
 @click.command()
 @click.option('--src-fname', type=str, help='the source filename')
 @click.option('--trg-fname', type=str, help='the target filename')
+@click.option('--low', type=float, default=0.1, help='len ratio < `low` will be shown, e.g: 0.1')
+@click.option('--high', type=float, default=9, help='len ratio > `high` will be shown, e.g: 9')
 @click.option('--encoding', type=str, default='utf-8', help='open and save encoding')
-def len_ratio(src_fname, trg_fname, encoding):
-    line_cnt = reader.count_lines(src_fname, encoding)
-    assert line_cnt == reader.count_lines(trg_fname, encoding), 'line count does not match...'
+def len_ratio(src_fname, trg_fname, low, high, encoding):
+    lines_cnt = reader.count_lines(src_fname, encoding)
+    assert lines_cnt == reader.count_lines(trg_fname, encoding), 'line count does not match...'
     ratio_stat = {}
+    illegal_cnt = 0
     with open(src_fname, 'r', encoding=encoding) as src_file, \
             open(trg_fname, 'r', encoding=encoding) as trg_file:
         for src_line, trg_line in zip(src_file, trg_file):
-            sen_len_ratio = math.ceil(len(trg_line.split()) / len(src_line.split()) * 10)
-            if sen_len_ratio < 6 or sen_len_ratio > 16:
+            sen_len_ratio = len(trg_line.split()) / len(src_line.split())
+            if sen_len_ratio < low or sen_len_ratio > high:
+                illegal_cnt += 1
                 print(src_line, trg_line)
+            sen_len_ratio = math.ceil(sen_len_ratio * 10) / 10
             if ratio_stat.get(sen_len_ratio) is not None:
                 ratio_stat[sen_len_ratio] += 1
             else:
                 ratio_stat[sen_len_ratio] = 1
     ratio_stat = list(ratio_stat.items())
-    assert line_cnt == sum(map(lambda x: x[1], ratio_stat))
+    assert lines_cnt == sum(map(lambda x: x[1], ratio_stat))
     ratio_stat.sort(key=lambda x: x[0])
     ratio_stat = dict(ratio_stat)
     for ratio, cnt in ratio_stat.items():
-        print(ratio, cnt)
+        print('('+str(ratio-0.1)+', '+str(ratio)+']', cnt)
+    print('=================================================')
+    print('low =', low, '| high =', high)
+    print('illegal sentences count:', illegal_cnt)
     tools.plot_bargraph('trg len ratio to src len distribution', ratio_stat)
 
 
