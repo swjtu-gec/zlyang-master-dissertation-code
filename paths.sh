0b4ffbbca39c046aecd888aae7b18caf30d31aa5
yangzonglin <806765278@qq.com>
2019-03-19 22:27:59 +0800

    beautify code

--- paths.sh
+++ paths.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
-DATA_DIR=$BASE_DIR/data
-MODEL_DIR=$BASE_DIR/models
-SCRIPTS_DIR=$BASE_DIR/scripts
-SOFTWARE_DIR=$BASE_DIR/software
+DATA_DIR=${BASE_DIR}/data
+MODEL_DIR=${BASE_DIR}/models
+SCRIPTS_DIR=${BASE_DIR}/scripts
+SOFTWARE_DIR=${BASE_DIR}/software
 
 # replace it with your fairseq path
 FAIRSEQPY=/home/yangzl/Desktop/pycharm-projects/fairseq
