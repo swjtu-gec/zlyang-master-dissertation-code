01120edb6d924320870b16e8e0c47b255aabbb49
yangzonglin <806765278@qq.com>
2019-03-20 23:15:28 +0800

    also download specify version of m2scorer

--- software/download.sh
+++ software/download.sh
@@ -1,20 +1,16 @@
-#COMMIT=ceb2f1200c9e5b8bf42a1033e7638d3e8586609a
-#echo "Downloading Fairseq from https://github.com/shamilcm/fairseq-py (rev:$COMMIT)"
-#wget https://github.com/shamilcm/fairseq-py/archive/$COMMIT.zip
-#unzip $COMMIT.zip
-#rm $COMMIT.zip
-#mv fairseq-py-$COMMIT fairseq-py
-#
-#COMMIT=b9453d5a211fc8f90fb25a584b39d4784f8de716
-#echo "Downloading n-best reranker from https://github.com/nusnlp/nbest-reranker (rev: $COMMIT)"
-#wget https://github.com/nusnlp/nbest-reranker/archive/$COMMIT.zip
-#unzip $COMMIT.zip
-#rm $COMMIT.zip
-#mv nbest-reranker-$COMMIT nbest-reranker
-##git clone https://github.com/nusnlp/nbest-reranker/
+#!/usr/bin/env bash
 
-echo "Downloading Subword NMT from https://github.com/rsennrich/subword-nmt (rev: ec5c7b009c409e72b5ef65a77c1a846546f14847)"
-wget https://github.com/rsennrich/subword-nmt/archive/ec5c7b009c409e72b5ef65a77c1a846546f14847.zip
-unzip ec5c7b009c409e72b5ef65a77c1a846546f14847.zip
-rm ec5c7b009c409e72b5ef65a77c1a846546f14847.zip
-mv subword-nmt-ec5c7b009c409e72b5ef65a77c1a846546f14847 subword-nmt
+commit=ec5c7b009c409e72b5ef65a77c1a846546f14847
+echo "Downloading Subword NMT from https://github.com/rsennrich/subword-nmt (rev: ${commit})"
+wget https://github.com/rsennrich/subword-nmt/archive/${commit}.zip
+unzip ${commit}.zip
+rm ${commit}.zip
+mv subword-nmt-${commit} subword-nmt
+
+commit=cb874a7ecdcb360b08635538a299cf868258a4c1
+echo "Downloading m2scorer from https://github.com/nusnlp/m2scorer (rev: ${commit})"
+wget https://github.com/nusnlp/m2scorer/archive/${commit}.zip
+unzip ${commit}.zip
+rm ${commit}.zip
+mv m2scorer-${commit} m2scorer
+
