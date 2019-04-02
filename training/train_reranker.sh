c01d41ea17823f6dcba4a9a48d1e207c60517e4d
yangzonglin <806765278@qq.com>
2019-04-02 19:58:46 +0800

    support non-bpe mode

--- training/train_reranker.sh
+++ training/train_reranker.sh
@@ -24,7 +24,7 @@ if [[ $# -ge 8 ]]; then
         fi
     fi
 else
-    echo "Usage: `basename $0` <dir to dev data> <output_dir> <GPU device id to use(e.g: '0, 1, 2')> <path to model_file/dir> <features, e.g: 'eo' or 'lm' or 'eolm'> <path-to-moses> <dir to bin data> <dir to BPE model> [optional args: <trained language model's url>]"
+    echo "Usage: `basename $0` <dir to dev data> <output_dir> <GPU device id to use(e.g: 0)> <path to model_file/dir> <features, e.g: 'eo' or 'lm' or 'eolm'> <path-to-moses> <dir to bin data> <dir to BPE model> [optional args: <trained language model's url>]"
     exit -1
 fi
 
@@ -47,6 +47,7 @@ elif [[ ! -e "$model_path" ]]; then
     exit -3
 fi
 
+
 ###############
 # training
 ###############
@@ -64,6 +65,9 @@ elif [[ "${reranker_feats}" == "eolm" ]]; then
 elif [[ "${reranker_feats}" == "lm" ]]; then
     echo "LM0= 0.5" >> ${TRAIN_DIR}/rerank_config.ini
     echo "WordPenalty0= -1" >> ${TRAIN_DIR}/rerank_config.ini
+else
+    echo "Unknown re-ranker features string, got ${reranker_feats}"
+    exit -4
 fi
 
 if [[ "${reranker_feats}" == "eo" ]]; then
@@ -72,22 +76,29 @@ elif [[ "${reranker_feats}" == "lm" ]]; then
     featstring="LM('LM0', '$lm_url', normalize=False), WordPenalty(name='WordPenalty0')"
 elif [[ "${reranker_feats}" == "eolm" ]]; then
     featstring="EditOps(name='EditOps0'), LM('LM0', '$lm_url', normalize=False), WordPenalty(name='WordPenalty0')"
-else
-    echo "Unknown re-ranker features string. got ${reranker_feats}"
-    exit -4
 fi
+
 
 train_reranker_starttime=$(date +%s)
 
-${SCRIPTS_DIR}/apply_bpe.py -c ${BPE_MODEL_DIR}/train.bpe.model < ${dev_data_dir}/dev.input.txt > ${output_dir}/dev.input.bpe.txt
+if [[ -d "$BPE_MODEL_DIR" ]]; then
+    ${SCRIPTS_DIR}/apply_bpe.py -c ${BPE_MODEL_DIR}/train.bpe.model < ${dev_data_dir}/dev.input.txt > ${output_dir}/dev.input.bpe.txt
+    beam_search_input=${output_dir}/dev.input.bpe.txt
+else
+    beam_search_input=${dev_data_dir}/dev.input.txt
+fi
 
 CUDA_VISIBLE_DEVICES="${device}" python ${FAIRSEQPY}/interactive.py \
     --no-progress-bar --path ${models} \
     --beam ${beam} --nbest ${beam} \
-    ${DATA_BIN_DIR} < ${output_dir}/dev.input.bpe.txt > ${output_dir}/dev.output.bpe.nbest.txt
+    ${DATA_BIN_DIR} < ${beam_search_input} > ${output_dir}/beamsearch.output.nbest.txt
 
 # reformating the nbest file
-${SCRIPTS_DIR}/nbest_reformat.py -i ${output_dir}/dev.output.bpe.nbest.txt --debpe > ${output_dir}/dev.output.tok.nbest.reformat.txt
+if [[ -d "$BPE_MODEL_DIR" ]]; then
+    ${SCRIPTS_DIR}/nbest_reformat.py -i ${output_dir}/beamsearch.output.nbest.txt --debpe > ${output_dir}/dev.output.tok.nbest.reformat.txt
+else
+    ${SCRIPTS_DIR}/nbest_reformat.py -i ${output_dir}/beamsearch.output.nbest.txt > ${output_dir}/dev.output.tok.nbest.reformat.txt
+fi
 
 # augmenting the dev nbest
 ${NBEST_RERANKER}/augmenter.py -s ${dev_data_dir}/dev.input.txt -i ${output_dir}/dev.output.tok.nbest.reformat.txt -o ${output_dir}/dev.output.tok.nbest.reformat.augmented.txt -f "$featstring"
