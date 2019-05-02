b7f634d259c5443d85306346517910e727200acc
yangzonglin <806765278@qq.com>
2019-05-02 21:28:38 +0800

    general script used to train LSTM seq2seq GEC model

---
+++ training/train_lstm.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+## This script used to train LSTM seq2seq NMT GEC model
+
+set -e
+set -x
+
+source ../paths.sh
+
+if [[ $# != 6 ]]; then
+    echo "Usage: `basename $0` <dir to bin data> <GPU device id to use(e.g: 0)> <model_name(e.g: fconv_zh_bpe)> <random seed> <max tokens> <max sentences>"
+    exit -1
+fi
+
+DATA_BIN_DIR=$1
+gpu_to_use=$2
+model_name=$3
+SEED=$4
+MAX_TOKENS=$5
+MAX_SENS=$6
+
+OUT_DIR=models/${model_name}/model${SEED}/
+mkdir -p ${OUT_DIR}
+
+CUDA_VISIBLE_DEVICES="${gpu_to_use}" python ${FAIRSEQPY}/train.py \
+    ${DATA_BIN_DIR} \
+    --save-dir ${OUT_DIR} \
+    -a lstm \
+    --encoder-embed-dim=1000 --decoder-embed-dim=1000 \
+    --encoder-hidden-size=500 --decoder-hidden-size=1000 \
+    --encoder-bidirectional --encoder-layers=2 --decoder-layers=2 \
+    --decoder-out-embed-dim=1000 \
+    --fp16 --fp16-init-scale=128 \
+    --num-workers=4 --skip-invalid-size-inputs-valid-test \
+    --max-epoch 100 \
+    --max-tokens ${MAX_TOKENS} --max-sentences ${MAX_SENS} \
+    --no-progress-bar --seed ${SEED}
+
