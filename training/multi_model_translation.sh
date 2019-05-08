eaf052b7a083de89e3cc0d50c3fbbdf40315e640
yangzonglin <806765278@qq.com>
2019-05-09 00:11:08 +0800

    specify which channels to use for correction

--- training/multi_model_translation.sh
+++ training/multi_model_translation.sh
@@ -11,7 +11,7 @@ encoding='utf-8'
 beam=12
 
 
-if [[ $# -eq 10 ]]; then
+if [[ $# -eq 11 ]]; then
     input_file=$1
     output_dir=$2
     device=$3
@@ -20,11 +20,25 @@ if [[ $# -eq 10 ]]; then
     bpe_model_path=$6
     BPE_BIN_DATA_DIR=$7
     BPE_MODEL_DIR=$8
-    channel_output_mode=$9
-    force_redo_translation=${10}
+    use_which_channels=$9
+    channel_output_mode=${10}
+    force_redo_translation=${11}
 else
-    echo "Usage: `basename $0` <input_file> <output_dir> <GPU device id to use(e.g: 0)> <path to char level model file/dir> <dir to bin data of char level model> <path to bpe level model file/dir> <dir to bin data of bpe level model> <dir to BPE model> <channel output mode, e.g: 1-best or n-best> <whether to force redo translation(e.g: false)>"
+    echo "Usage: `basename $0` <input_file> <output_dir> <GPU device id to use(e.g: 0)> <path to char level model file/dir> <dir to bin data of char level model> <path to bpe level model file/dir> <dir to bin data of bpe level model> <dir to BPE model> <use which channels, e.g: '1 1 0 0'> <channel output mode, e.g: 1-best or n-best> <whether to force redo translation(e.g: false)>"
     exit -1
+fi
+
+
+used_channels=(${use_which_channels})
+use_M1=${used_channels[0]}
+use_M2=${used_channels[1]}
+if [[ ${use_M2} -ne 0 ]]; then
+    use_M1=1
+fi
+use_M3=${used_channels[2]}
+use_M4=${used_channels[3]}
+if [[ ${use_M4} -ne 0 ]]; then
+    use_M3=1
 fi
 
 
@@ -73,25 +87,27 @@ sed 's/ //g' ${input_file} > ${output_dir}/input.sen.txt
 # ============================
 # M1 channel: char level model
 # ============================
-M1_output=${output_dir}/M1.fairseq.output.nbest.txt
+M1_output=${output_dir}/M1.fairseq.output.${channel_output_mode}.txt
 if [[ ! -e ${M1_output} || ${force_redo_translation} == true ]]; then
-    # prepare input for M1 channel
-    M1_input=${output_dir}/M1.input.char.txt
-    python ${segment_py} --raw-fname=${output_dir}/input.sen.txt --seg-fname=${M1_input} --char-level --encoding=${encoding}
+    if [[ ${use_M1} -ne 0 ]]; then
+        # prepare input for M1 channel
+        M1_input=${output_dir}/M1.input.char.txt
+        python ${segment_py} --raw-fname=${output_dir}/input.sen.txt --seg-fname=${M1_input} --char-level --encoding=${encoding}
 
-    CUDA_VISIBLE_DEVICES="${device}" python ${FAIRSEQPY}/interactive.py \
-        --no-progress-bar \
-        --path ${char_models} \
-        --beam ${beam} --nbest ${nbest} \
-        --model-overrides "{'encoder_embed_path': None, 'decoder_embed_path': None}" \
-        ${CHAR_BIN_DATA_DIR} < ${M1_input} > ${M1_output}
+        CUDA_VISIBLE_DEVICES="${device}" python ${FAIRSEQPY}/interactive.py \
+            --no-progress-bar \
+            --path ${char_models} \
+            --beam ${beam} --nbest ${nbest} \
+            --model-overrides "{'encoder_embed_path': None, 'decoder_embed_path': None}" \
+            ${CHAR_BIN_DATA_DIR} < ${M1_input} > ${M1_output}
+    fi
 fi
 
 # ==============================================
 # M2 channel: char level model + bpe level model
 # ==============================================
-M2_output=${output_dir}/M2.fairseq.output.nbest.txt
-if [[ ! -e ${M2_output} || ${force_redo_translation} == true ]]; then
+M2_output=${output_dir}/M2.fairseq.output.${channel_output_mode}.txt
+if [[ ${use_M2} -ne 0 ]] && [[ ! -e ${M2_output} || ${force_redo_translation} == true ]]; then
     M2_input=${output_dir}/M2.input.bpe.txt
     # getting best hypotheses for M2 input
     cat ${M1_output} | grep "^H"  | \
@@ -111,8 +127,8 @@ fi
 # ===========================
 # M3 channel: BPE level model
 # ===========================
-M3_output=${output_dir}/M3.fairseq.output.nbest.txt
-if [[ ! -e ${M3_output} || ${force_redo_translation} == true ]]; then
+M3_output=${output_dir}/M3.fairseq.output.${channel_output_mode}.txt
+if [[ ${use_M3} -ne 0 ]] && [[ ! -e ${M3_output} || ${force_redo_translation} == true ]]; then
     # prepare input for M3 channel
     M3_input=${output_dir}/M3.input.bpe.txt
     python ${segment_py} --raw-fname=${output_dir}/input.sen.txt --seg-fname=${output_dir}/M3.input.jieba.txt --encoding=${encoding}
@@ -129,8 +145,8 @@ fi
 # ==============================================
 # M4 channel: bpe level model + char level model
 # ==============================================
-M4_output=${output_dir}/M4.fairseq.output.nbest.txt
-if [[ ! -e ${M4_output} || ${force_redo_translation} == true ]]; then
+M4_output=${output_dir}/M4.fairseq.output.${channel_output_mode}.txt
+if [[ ${use_M4} -ne 0 ]] && [[ ! -e ${M4_output} || ${force_redo_translation} == true ]]; then
     # prepare input for M4 channel
     M4_input=${output_dir}/M4.input.char.txt
     cat ${M3_output} | grep "^H"  | \
