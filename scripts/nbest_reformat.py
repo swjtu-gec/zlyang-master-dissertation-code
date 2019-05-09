ba80f40e3cd7bf58c0c34a053ba6f056e37293f1
yangzonglin <806765278@qq.com>
2019-05-10 05:00:11 +0800

    supports re-segmentation of hypotheses in char level when re-formatting fairseq's output

--- scripts/nbest_reformat.py
+++ scripts/nbest_reformat.py
@@ -1,10 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import argparse
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(PROJECT_ROOT)
+
+from data.segment import segment_sen
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input-file',  help='path to input file (output of fairseq)')
 parser.add_argument('--debpe',  action='store_true', help='enable the flag to post-process and remove BPE segmentation.')
+parser.add_argument('--char-seg', action='store_true', help='whether to re-seg sentence in char level')
 
 args = parser.parse_args()
 
@@ -20,7 +28,9 @@ with open(args.input_file) as f:
             hyp = pieces[2]
             if args.debpe:
                 hyp = hyp.replace('@@ ','')
+            if args.char_seg:
+                hyp = hyp.replace(' ', '')
+                hyp = ' '.join(segment_sen(hyp, True, False))
             score = pieces[1]
             print("%d ||| %s ||| F0= %s ||| %s" % (scount, hyp, score, score) )
-
 
