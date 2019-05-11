57c21a6a3d085149ae6ea302f505fc6d2bd2dccd
yangzonglin <806765278@qq.com>
2019-05-11 18:17:27 +0800

    add `multi_channel_fusion_experiment.sh` bash script for multi-model-fusion experiments

---
+++ training/multi_channel_fusion_experiments.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+source ../paths.sh
+
+############################
+# modify following arguments
+############################
+device=0
+use_which_channels='1 1 1 1'
+channel_output_mode=n-best
+force_redo_translation=false
+char_model_path=models/4_ens_top/
+bpe_model_path=models/4_ens_fconv_bpe_word2vec_2_4_5_6/
+reranker_feats=eo+lm
+
+
+#################
+# fixed arguments
+#################
+train_multi_channel_fusion_reranker_sh=${BASE_DIR}/training/train_multi_channel_fusion_reranker.sh
+multi_channel_fusion_sh=${BASE_DIR}/training/multi_channel_fusion.sh
+remove_spac_pkunlp_segment_sh=${BASE_DIR}/training/remove_spac_pkunlp_segment.sh
+
+char_dev_data_dir=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+moses_path=../../mosesdecoder/
+CHAR_BIN_DATA_DIR=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
+BPE_BIN_DATA_DIR=zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
+BPE_MODEL_DIR=models/zh_bpe_model_NLPCC_betterseg+HSK_remove_short1_long1000_low0.1_high200.0/
+if [[ -d ${char_model_path} ]]; then
+    inference_mode=4ens
+else
+    inference_mode=single
+fi
+multi_channel_fusion_train_reranker_output_dir=../eval/multi_channel_fusion_train_reranker_${inference_mode}_${channel_output_mode/-/}/
+char_lm_url=models/lm/wiki_zh.char.5gram.binary.trie
+
+test_input_char_file=../data/test/nlpcc2018-test/char.seg.txt
+multi_channel_translation_output_dir=../eval/multi_channel_translation_${inference_mode}_${channel_output_mode/-/}/
+reranker_weights=${multi_channel_fusion_train_reranker_output_dir}/${reranker_feats}/weights.${reranker_feats}.txt
+
+gold_edit=${BASE_DIR}/data/test/nlpcc2018-test/gold.01
+m2scorer_url=${BASE_DIR}/eval/m2scorer/scripts/m2scorer.py
+gec_system_output=${multi_channel_translation_output_dir}/${reranker_feats}/output.char.merged.reranked.txt
+
+
+##############################################################################
+# training re-ranker component => multi channel fusion => evaluate performance
+##############################################################################
+if [[ "${reranker_feats}" == "eo-feats" || "${reranker_feats}" == "lm-feats" || \
+        ${reranker_feats} == "lm-feats-normalized" || "${reranker_feats}" == "eo+lm" ]]; then
+    ${train_multi_channel_fusion_reranker_sh} \
+        ${char_dev_data_dir} ${multi_channel_fusion_train_reranker_output_dir} \
+        ${device} "$use_which_channels" ${channel_output_mode} ${force_redo_translation} ${moses_path} \
+        ${char_model_path} ${CHAR_BIN_DATA_DIR} ${bpe_model_path} ${BPE_BIN_DATA_DIR} ${BPE_MODEL_DIR} \
+        ${reranker_feats} ${char_lm_url}
+fi
+
+${multi_channel_fusion_sh} ${test_input_char_file} ${multi_channel_translation_output_dir} \
+    ${device} "$use_which_channels" ${channel_output_mode} ${force_redo_translation} \
+    ${char_model_path} ${CHAR_BIN_DATA_DIR} ${bpe_model_path} ${BPE_BIN_DATA_DIR} ${BPE_MODEL_DIR} \
+    ${reranker_feats} ${reranker_weights} ${char_lm_url}
+
+${remove_spac_pkunlp_segment_sh} ${m2scorer_url} ${gec_system_output} ${gold_edit} false
+
