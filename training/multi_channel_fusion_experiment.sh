db38e5cee86d100a5037761b47797e9b576822d6
yangzonglin <806765278@qq.com>
2019-06-17 04:33:44 +0800

    use absolute url and optimize the code

--- training/multi_channel_fusion_experiment.sh
+++ training/multi_channel_fusion_experiment.sh
@@ -26,21 +26,24 @@ train_multi_channel_fusion_reranker_sh=${BASE_DIR}/training/train_multi_channel_
 multi_channel_fusion_sh=${BASE_DIR}/training/multi_channel_fusion.sh
 remove_spac_pkunlp_segment_sh=${BASE_DIR}/training/remove_spac_pkunlp_segment.sh
 
-char_dev_data_dir=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+bpe_processed_dir=${BASE_DIR}/training/zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+char_processed_dir=${BASE_DIR}/training/zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+
+char_dev_data_dir=${char_processed_dir}
 moses_path=../../mosesdecoder/
-CHAR_BIN_DATA_DIR=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
-BPE_BIN_DATA_DIR=zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
-BPE_MODEL_DIR=models/zh_bpe_model_NLPCC_betterseg+HSK_remove_short1_long1000_low0.1_high200.0/
+CHAR_BIN_DATA_DIR=${char_processed_dir}/bin/
+BPE_BIN_DATA_DIR=${bpe_processed_dir}/bin/
+BPE_MODEL_DIR=${BASE_DIR}/training/models/zh_bpe_model_NLPCC_betterseg+HSK_remove_short1_long1000_low0.1_high200.0/
 if [[ -d ${char_model_path} ]]; then
     inference_mode=4ens
 else
     inference_mode=single
 fi
-multi_channel_fusion_train_reranker_output_dir=../eval/multi_channel_fusion_train_reranker_${inference_mode}_${channel_output_mode/-/}/
-char_lm_url=models/lm/wiki_zh.char.5gram.binary.trie
+multi_channel_fusion_train_reranker_output_dir=${BASE_DIR}/eval/multi_channel_fusion_train_reranker_${inference_mode}_${channel_output_mode/-/}/
+char_lm_url=${BASE_DIR}/training/models/lm/wiki_zh.char.5gram.binary.trie
 
-test_input_char_file=../data/test/nlpcc2018-test/char.seg.txt
-multi_channel_translation_output_dir=../eval/multi_channel_translation_${inference_mode}_${channel_output_mode/-/}/
+test_input_char_file=${BASE_DIR}/data/test/nlpcc2018-test/char.seg.txt
+multi_channel_translation_output_dir=${BASE_DIR}/eval/multi_channel_translation_${inference_mode}_${channel_output_mode/-/}/
 reranker_weights=${multi_channel_fusion_train_reranker_output_dir}/${reranker_feats}/weights.${reranker_feats}.txt
 
 gold_edit=${BASE_DIR}/data/test/nlpcc2018-test/gold.01
