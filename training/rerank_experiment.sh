6eebd8eb4b2a084531394d3dd873e9be1432bd7a
yangzonglin <806765278@qq.com>
2019-06-17 00:24:16 +0800

    use absolute url, optimize the code and fix bug

--- training/rerank_experiment.sh
+++ training/rerank_experiment.sh
@@ -16,14 +16,17 @@ remove_spac_pkunlp_segment_sh=${BASE_DIR}/training/remove_spac_pkunlp_segment.sh
 jieba_seg_test_input=${BASE_DIR}/data/test/nlpcc2018-test/jieba.seg.txt
 char_seg_test_input=${BASE_DIR}/data/test/nlpcc2018-test/char.seg.txt
 
-word_dev_data_dir=zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0
-char_dev_data_dir=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+bpe_processed_dir=${BASE_DIR}/training/zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+char_processed_dir=${BASE_DIR}/training/zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/
+
+word_dev_data_dir=${bpe_processed_dir}
+char_dev_data_dir=${char_processed_dir}
 moses_path=../../mosesdecoder/
 
-BPE_BIN_DATA_DIR=zh-bpe-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
-CHAR_BIN_DATA_DIR=zh-char-processed-NLPCC_betterseg+HSK-remove-short1-long1000-low0.1-high200.0/bin/
+BPE_BIN_DATA_DIR=${bpe_processed_dir}/bin/
+CHAR_BIN_DATA_DIR=${char_processed_dir}/bin/
 
-char_lm_url=models/lm/wiki_zh.char.5gram.binary.trie
+char_lm_url=${BASE_DIR}/training/models/lm/wiki_zh.char.5gram.binary.trie
 word_lm_url=${BASE_DIR}/training/models/lm/wiki_zh.word.5gram.binary.trie
 
 gold_edit=${BASE_DIR}/data/test/nlpcc2018-test/gold.01
@@ -39,12 +42,12 @@ device=0
 # eo, lm, eolm
 reranker_feats=eolm
 dev_data_dir=${word_dev_data_dir}
-model_path=${BASE_DIR}/training/models/4_ens_fconv_bpe_word2vec_2_4_5_6
-BPE_MODEL_DIR=models/zh_bpe_model_NLPCC_betterseg+HSK_remove_short1_long1000_low0.1_high200.0/
-train_reranker_output_dir=../eval/${model_path##*/}_train_reranker_${reranker_feats}
+model_path=${BASE_DIR}/training/models/fconv_bpe_word2vec_4ens_2_4_5_6
+BPE_MODEL_DIR=${BASE_DIR}/training/models/zh_bpe_model_NLPCC_betterseg+HSK_remove_short1_long1000_low0.1_high200.0/
+train_reranker_output_dir=${BASE_DIR}/eval/${model_path##*/}_train_reranker_${reranker_feats}
 DATA_BIN_DIR=${BPE_BIN_DATA_DIR}
 test_input=${jieba_seg_test_input}
-run_models_output_dir=../eval/${model_path##*/}_${reranker_feats}
+run_models_output_dir=${BASE_DIR}/eval/${model_path##*/}_${reranker_feats}
 reranker_weights=${train_reranker_output_dir}/weights.${reranker_feats}.txt
 lm_url=${word_lm_url}
 gec_system_output=${run_models_output_dir}/output.tok.txt
@@ -55,12 +58,12 @@ gec_system_output=${run_models_output_dir}/output.tok.txt
 ## eo, lm, eolm
 #reranker_feats=eolm
 #dev_data_dir=${char_dev_data_dir}
-#model_path=${BASE_DIR}/training/models/4_ens_fconv_char_random_1_2_4_1000
+#model_path=${BASE_DIR}/training/models/fconv_char_random_4ens_1_2_4_1000
 #BPE_MODEL_DIR=None
-#train_reranker_output_dir=../eval/${model_path/##*/}_train_reranker_${reranker_feats}
+#train_reranker_output_dir=${BASE_DIR}/eval/${model_path/##*/}_train_reranker_${reranker_feats}
 #DATA_BIN_DIR=${CHAR_BIN_DATA_DIR}
 #test_input=${char_seg_test_input}
-#run_models_output_dir=../eval/${model_path/##*/}_${reranker_feats}
+#run_models_output_dir=${BASE_DIR}/eval/${model_path/##*/}_${reranker_feats}
 #reranker_weights=${train_reranker_output_dir}/weights.${reranker_feats}.txt
 #lm_url=${char_lm_url}
 #gec_system_output=${run_models_output_dir}/output.tok.txt
